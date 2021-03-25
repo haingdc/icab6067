a98cc6256f8903bb22e5c6c48872d62682846c89
hai nguyen <haingdc@gmail.com>
2021-03-25 22:17:14 +0700

    update

--- packages/mobile/ios/myprojectname/AppDelegate.m
+++ packages/mobile/ios/myprojectname/AppDelegate.m
@@ -23,10 +23,13 @@ static void InitializeFlipper(UIApplication *application) {
 }
 #endif
 
+#import <GoogleMaps/GoogleMaps.h>
+
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+[GMSServices provideAPIKey:@"AIzaSyA_lsmHKQ5FTBzSHFlJXWqqSQxHfuvM8Lc"];
 #ifdef FB_SONARKIT_ENABLED
   InitializeFlipper(application);
 #endif
