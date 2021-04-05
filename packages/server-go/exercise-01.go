391340929f1a831368ef113f68d9cb8eebfdae3e
hai nguyen <haingdc@gmail.com>
2021-04-05 22:12:54 +0700

    try 2

--- packages/server-go/exercise-01.go
+++ packages/server-go/exercise-01.go
@@ -1,7 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"reflect"
+)
 
 func main() {
-	fmt.Printf("Hello everyone, my name is %s. This is my first course about GO lang. They say this language is very interesting and it's %t", "Hai", true)
+	// fmt.Printf("Hello everyone, my name is %s. This is my first course about GO lang. They say this language is very interesting and it's %t", "Hai", true)
+	fmt.Println(reflect.TypeOf("Brenna"))
 }
