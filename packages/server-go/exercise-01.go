2cac5df6fe0d9acc4645fe3de2848507fcbb9ed3
hai nguyen <haingdc@gmail.com>
2021-04-05 22:05:31 +0700

    first go lesson

---
+++ packages/server-go/exercise-01.go
@@ -0,0 +1,7 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Printf("Hello everyone, my name is %s. This is my first course about GO lang. They say this language is very interesting and it's %t", "Hai", true)
+}
